af9802d71e9a806fc4005ce3d486bc7bb8078b3b
Fxvxnn <fynn-assmann@gmx.de>
2024-03-09 10:14:09 +0100

    modified:   math_calc/__pycache__/algorithms.cpython-311.pyc 	modified:   math_calc/algorithms.py 	modified:   math_calc/v01.py

--- math_calc/algorithms.py
+++ math_calc/algorithms.py
@@ -1,9 +1,9 @@
 from math import *
 
-def bisektion(f, y_0: float, a: float, b: float, eps: float = 0.0001):
+def bisektion(f, y_0: float, a: float, b: float, eps: float = 0.000001):
     while abs(a-b) >= eps:
         m = (a+b)/2
-        if f(a) * f(m) < y_0:
+        if (f(a)-y_0) * (f(m)-y_0) < 0:
             b = m
         else: a = m
-    return round(m, len(str(eps).split(".")[1]))
+    return round(m, 2)
--- math_calc/v01.py
+++ math_calc/v01.py
@@ -29,9 +29,6 @@ class Function:
             points.append((x, self(x)))
             x += acc
 
-    def get_root(self, min_x: float, max_x: float, acc: float = 0.1) -> float:
-        pass
-
     def call_y(self, x: float) -> float:
         """
         return y value for a given x value
@@ -44,24 +41,31 @@ class Function:
         """
         output = [] 
         points = self.draw(min_x, max_x, acc)
+
+        # first point of interval or window
         start = points[0]
+
+        # criterium to check if y_0 is between two points
         criterium = (lambda y: y < y_0) if start[1] > y_0 else (lambda y: y > y_0)
+
+        # iterate through points and find x values for y_0
         for p in points[1:]:
-            if p[1] == y_0: 
-                output.append(p)
-                start = p
-                criterium = (lambda y: y < y_0) if start[1] > y_0 else (lambda y: y > y_0)
-            elif criterium(p[1]):
-                print(p)
+            if criterium(p[1]):
                 x = bisektion(self, y_0, start[0], p[0])
                 output.append((x, y_0))
                 start = p
                 criterium = (lambda y: y < y_0) if start[1] > y_0 else (lambda y: y > y_0)
         return output
+    
+    def get_root(self, min_x: float, max_x: float, acc: float = 0.1) -> float:
+        """
+        returns root of a function in a given interval
+        """
+        return self.call_x(0, min_x, max_x, acc)
 
     
     
         
 
 f = Function(input("f(x) = "))
-print(f.call_x(4, -5, 5))
+print(f.get_root(-5, 5))
